21d3e836b054f870b23c3515254bf9d2a1978cd2
Sebastian Burkhardt <ga29rox@mytum.de>
2021-11-11 16:43:33 +0100

    added lists controller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(strong_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  def strong_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
